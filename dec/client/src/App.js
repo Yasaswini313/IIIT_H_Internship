69a35078cd4b3d3cbbde928018b55c6aed8485ee
Yasaswini313 <105104871+Yasaswini313@users.noreply.github.com>
2023-11-30 09:36:45 +0530

    Add files via upload

---
+++ dec/client/src/App.js
@@ -0,0 +1,198 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="home-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="ellipse" />
+            <img className="rectangle" alt="Rectangle" src="https://c.animaapp.com/3oUarRBs/img/rectangle-1.png" />
+            <img className="img" alt="Rectangle" src="https://c.animaapp.com/3oUarRBs/img/rectangle-3.png" />
+            <p className="text-wrapper">Unveiling the Wonders of the Universe</p>
+            <img
+              className="lost-in-space"
+              alt="Lost in space"
+              src="https://c.animaapp.com/3oUarRBs/img/lost-in-space-3d-cute-girl-in-astronaut-costume-transparent-back.png"
+            />
+            <div className="group">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-2">Exercising</div>
+                <div className="div-2">
+                  <img className="group-2" alt="Group" src="https://c.animaapp.com/3oUarRBs/img/group-113@2x.png" />
+                </div>
+                <img className="OBJECTS" alt="Objects" src="https://c.animaapp.com/3oUarRBs/img/objects-2.svg" />
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-2">
+                <div className="group-wrapper">
+                  <div className="overlap-group-wrapper">
+                    <div className="overlap-group-3">
+                      <div className="text-wrapper-3">352D 5H 49M 20S</div>
+                      <div className="text-wrapper-4">Total time in space</div>
+                      <div className="div-2" />
+                    </div>
+                  </div>
+                </div>
+                <div className="stopwatch" />
+              </div>
+            </div>
+            <div className="frame-wrapper">
+              <div className="frame">
+                <div className="overlap-3">
+                  <div className="div-wrapper">
+                    <div className="overlap-group-4">
+                      <div className="div-2" />
+                      <div className="text-wrapper-5">Spacewalks</div>
+                      <img
+                        className="OBJECTS-2"
+                        alt="Objects"
+                        src="https://c.animaapp.com/3oUarRBs/img/objects-1.svg"
+                      />
+                      <img className="OBJECTS-3" alt="Objects" src="https://c.animaapp.com/3oUarRBs/img/objects.svg" />
+                    </div>
+                  </div>
+                  <div className="chart-pie" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-2">
+            <div className="text-wrapper-6">Get Started</div>
+          </div>
+          <p className="at-odissey-our">
+            At odissey, our mission is to inspire, educate, and ignite your passion for the cosmos. We are your premier
+            destination for all things related to space exploration, astronomy, and the mysteries of the universe.
+            Whether you&#39;re a seasoned astronomer or a curious novice, join us on a journey to the furthest reaches
+            of the cosmos.
+          </p>
+          <div className="text-wrapper-7">Watch the Video</div>
+          <div className="play-wrapper">
+            <div className="play" />
+          </div>
+        </div>
+        <img className="odyssey" alt="Odyssey" src="https://c.animaapp.com/3oUarRBs/img/odyssey.svg" />
+        <div className="text-wrapper-8">Odyssey</div>
+        <div className="text-wrapper-9">About Us</div>
+        <div className="text-wrapper-10">Community</div>
+        <div className="text-wrapper-11">Education</div>
+        <div className="text-wrapper-12">Stay Connected</div>
+        <div className="text-wrapper-13">About Us</div>
+        <div className="text-wrapper-14">Community</div>
+        <div className="text-wrapper-15">Education</div>
+        <div className="text-wrapper-16">Stay Connected</div>
+        <div className="frame-3">
+          <div className="text-wrapper-17">Sign in</div>
+        </div>
+        <div className="group-3">
+          <img
+            className="spacex-logo"
+            alt="Spacex logo"
+            src="https://c.animaapp.com/3oUarRBs/img/spacex-logo-1@2x.png"
+          />
+          <img
+            className="element-NASA-worm"
+            alt="Element NASA worm"
+            src="https://c.animaapp.com/3oUarRBs/img/2560px-nasa-worm-logo--black--1@2x.png"
+          />
+          <img
+            className="boeing-logo"
+            alt="Boeing logo"
+            src="https://c.animaapp.com/3oUarRBs/img/boeing-logo-1960-present-1@2x.png"
+          />
+          <img
+            className="astroscale-main-logo"
+            alt="Astroscale main logo"
+            src="https://c.animaapp.com/3oUarRBs/img/astroscale-main-logo-rgb-1@2x.png"
+          />
+        </div>
+        <p className="p">Join our vibrant community of space enthusiasts where you can</p>
+        <p className="text-wrapper-18">
+          Dive into the basics of astronomy. Learn about stars, planets, galaxies, and the tools astronomers use to
+          explore the universe.
+        </p>
+        <div className="text-wrapper-19">Astronomy 101</div>
+        <p className="text-wrapper-20">
+          Stay up-to-date with the most recent breakthroughs and discoveries in the field of astrophysics and space
+          exploration.
+        </p>
+        <div className="text-wrapper-21">Latest Discoveries</div>
+        <p className="text-wrapper-22">
+          Explore the latest missions, both past and present, from NASA, ESA, SpaceX, and other space agencies and
+          organizations.
+        </p>
+        <div className="text-wrapper-23">Space Exploration</div>
+        <p className="text-wrapper-24">
+          Delve into the cutting-edge technology powering space exploration, from spacecraft and telescopes to
+          propulsion systems and space habitats.
+        </p>
+        <div className="text-wrapper-25">Space Technology</div>
+        <div className="physics-wrapper">
+          <div className="physics" />
+        </div>
+        <div className="telescope-wrapper">
+          <div className="telescope" />
+        </div>
+        <div className="clipboard-wrapper">
+          <div className="clipboard" />
+        </div>
+        <div className="text-wrapper-26">Privacy policy</div>
+        <div className="terms-agreements">Terms &amp; Agreements</div>
+        <p className="contact-odyssey-com">
+          <span className="span">[</span>
+          <a href="mailto:contact@cosmicexplorer.com" rel="noopener noreferrer" target="_blank">
+            <span className="text-wrapper-27">contact@odyssey.com</span>
+          </a>
+          <span className="span">].</span>
+        </p>
+        <div className="chart-pie-wrapper">
+          <img className="chart-pie-2" alt="Chart pie" src="https://c.animaapp.com/3oUarRBs/img/chart-pie@2x.png" />
+        </div>
+        <div className="overlap-4">
+          <img className="rectangle" alt="Rectangle" src="https://c.animaapp.com/3oUarRBs/img/rectangle-8.png" />
+          <p className="text-wrapper-28">
+            We are committed to making space science accessible to all. Explore our educational resources, including
+          </p>
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <p className="text-wrapper-29">
+            Dive into the basics of astronomy. Learn about stars, planets, galaxies, and the tools astronomers use to
+            explore the universe.
+          </p>
+          <div className="text-wrapper-30">01.</div>
+          <p className="text-wrapper-31">Online free courses and tutorials</p>
+          <p className="text-wrapper-32">
+            Delve into the cutting-edge technology powering space exploration, from spacecraft and telescopes to
+            propulsion systems and space habitats.
+          </p>
+          <div className="text-wrapper-33">02.</div>
+          <p className="text-wrapper-34">Resources for teachers and educators</p>
+          <p className="text-wrapper-35">
+            Dive into the basics of astronomy. Learn about stars, planets, galaxies, and the tools astronomers use to
+            explore the universe.
+          </p>
+          <div className="text-wrapper-36">03.</div>
+          <div className="text-wrapper-37">Book recommendations and reviews</div>
+        </div>
+        <div className="overlap-5">
+          <p className="text-wrapper-38">
+            of stargazers, explorers, and cosmic enthusiasts as we embark on a journey through the cosmos like never
+            before.
+          </p>
+          <div className="text-wrapper-39">Join our community</div>
+          <div className="frame-4">
+            <div className="text-wrapper-6">Subscribe</div>
+          </div>
+          <div className="frame-5">
+            <div className="text-wrapper-40">Enter your email</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
